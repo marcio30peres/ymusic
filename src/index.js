a10e9d005a19ad7f38a5e952e359cba7cb6101ab
Márcio <marcio30peres@gmail.com>
2022-03-08 09:36:26 -0300

    feat(add): discord client setup

--- src/index.js
+++ src/index.js
@@ -1,13 +1,41 @@
+const { Player } = require("discord-player")
+const Discord = require("discord.js")
 const express = require('express');
 const dotenv = require("dotenv")
 
+
 dotenv.config()
+const PORT = process.env.PORT
+const TOKEN = process.env.TOKEN
+
 const app = new express()
 
 app.get('/', (request, response) => {
     response.status(200).send()
 })
 
-app.listen(process.env.PORT)
+app.listen(PORT)
 
-console.log(`YMusic's running!`)
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES",
+        "GUILD_PRESENCES",
+        "GUILD_VOICE_STATES",
+        "GUILD_MESSAGE_REACTIONS"
+    ],
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+})
+
+client.player = new Player(client, {
+    ytdlOptions: {
+        quality: "highestaudio",
+        highWaterMark: 1 << 25
+    }
+})
+
+client.on("ready", () => {
+    console.log("YMusic's running!")
+})
+
+client.login(TOKEN)
